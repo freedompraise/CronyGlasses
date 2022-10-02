9d2917336db8f7d2a4e3e4886c92d344d4989e31
freedompraise <dikepraise119@gmail.com>
2022-10-02 20:34:29 +0100

    models init

--- service/models.py
+++ service/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Drinks(models):
+    name = models.CharField(max_length=20, unique = True)
+    type = models.CharField(max_length=20)
+    size = moels.CharField(max_length = 4)
