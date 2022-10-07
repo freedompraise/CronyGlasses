fcb8a3777fa0df8500b0ec34d53da3cdec7ad4e0
freedompraise <dikepraise119@gmail.com>
2022-10-07 19:27:30 +0100

    Backend Branch Initiated

--- service/models.py
+++ service/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Drinks(models.Model):
+class Drink(models.Model):
     name = models.CharField(max_length=20, unique = True)
-    type = models.CharField(max_length=20)
-    size = models.CharField(max_length = 4)
+    type = models.CharField(max_length=20, blank=True)
+    size = models.CharField(max_length = 5)
+    customer = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
