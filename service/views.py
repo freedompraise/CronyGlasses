6e7b5459d44672de0c57230049a64ad345827db7
freedompraise <dikepraise119@gmail.com>
2022-10-08 13:56:57 +0100

    DB query init

--- service/views.py
+++ service/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index(request):
+    drinks = Drink.objects.all()[:8]
+
     return render(request,'service/index.html')
