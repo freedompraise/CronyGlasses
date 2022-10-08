595ea340f33736c4e60824b87bcf915e9fdc7568
freedompraise <dikepraise119@gmail.com>
2022-10-08 05:59:06 +0100

    revert 0001 attempt

--- cronyGlasses/settings.py
+++ cronyGlasses/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
