1b4c4ee9cbc976f2190ee5327d2f627e4b2439b6
Praise Freedom Dike <dikepraise119@gmail.com>
2023-02-28 15:18:33 +0100

    added backend code for login

--- service/views.py
+++ service/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -7,3 +9,18 @@ def index(request):
     drinks = Drink.objects.all()[:8]
 
     return render(request,'service/index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') # Replace 'home' with the URL name of your homepage
+        else:
+            messages.error(request, 'Invalid email or password.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'service/login.html', {'form': form})
