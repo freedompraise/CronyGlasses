7fab08399b4cb70fa30f72533dc48507432f3ab2
Praise Freedom Dike <dikepraise119@gmail.com>
2023-03-06 04:41:35 +0100

    add_to_cart url added to service urls

--- service/urls.py
+++ service/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('auth/register/',views.register,name='register'),
     path('cart/', views.cart, name='cart'),
     path('drinks/<int:pk>/',views.product_page, name='drinks'),
+    path('cart/<int:drink_id>/add/', views.add_to_cart, name='add_to_cart'),
     path('cart/<int:drink_id>/update/', views.cart_update, name='cart_update'),
     path('<int:drink_id>/remove/', views.cart_remove, name='cart_remove'),
     path('checkout/', views.checkout, name='checkout'),
-    path('paypal_checkout/', views.paypal_checkout, name='paypal_checkout'),
+    path('paypal/', views.paypal_checkout, name='paypal_checkout'),
     path('payment-done/', views.payment_done, name='payment_done'),
-    path('payment-cancelled/', views.payment_cancelled, name="payment_cancelled")
+    path('payment-cancelled/', views.payment_cancelled, name="payment_cancelled"),
     ]
