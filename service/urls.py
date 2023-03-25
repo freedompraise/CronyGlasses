1fda4f969ac70adb283f4b321257c20d4705d123
Praise Freedom Dike <dikepraise119@gmail.com>
2023-03-25 12:31:05 +0100

    change payment-done urls name

--- service/urls.py
+++ service/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('remove/<int:order_item_id>/', views.cart_remove, name='cart_remove'),
     path('checkout/', views.checkout, name='checkout'),
     path('paypal_checkout/', views.paypal_checkout, name='paypal_checkout'),
-    path('payment-done/', views.payment_done, name='payment-done'),
-    path('payment-cancelled/', views.payment_cancelled, name="payment-cancelled"),
+    path('payment_done/', views.payment_done, name='payment-done'),
+    path('payment_cancelled/', views.payment_cancelled, name="payment-cancelled"),
     ]
