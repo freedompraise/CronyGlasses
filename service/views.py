9c1aff46f97a962d293bd4e7ea126fb65424d23b
Praise Freedom Dike <dikepraise119@gmail.com>
2023-03-25 08:40:26 +0100

    refactor casrt_remove views

--- service/views.py
+++ service/views.py
@@ -108,15 +108,10 @@ def cart(request):
 @login_required
 def cart_remove(request, order_item_id):
     # Get the Cart object for the current user
-    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart = Cart.objects.get(user=request.user)
     # Get the OrderItem object from the database
     order_item = get_object_or_404(OrderItem, id=order_item_id)
-    if order_item:
-        cart.order_items.remove(order_item)
-        cart.save()
-        messages.success(request, f"{order_item.drink.name} has been removed from your cart.")
-    else:
-        messages.warning(request, "The selected item is not in your cart.")
+    cart.order_items.remove(order_item)
     cart.save()
 
     return redirect('cart')
