b1d0a08a7a6f9d9c39dc657856881f375b76894b
Praise Freedom Dike <dikepraise119@gmail.com>
2023-03-30 16:25:39 +0100

    attempt to fix paypal

--- cronyGlasses/urls.py
+++ cronyGlasses/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from paypal.standard.ipn import views as paypal_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('service.urls')),
-    path('paypal/', include('paypal.standard.ipn.urls')),
+    path('paypal-ipn/', paypal_views.ipn, name='paypal-ipn'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
