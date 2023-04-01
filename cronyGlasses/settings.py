67822723d3112b6906d5f405901abc88e2366a7d
Praise Freedom Dike <dikepraise119@gmail.com>
2023-04-01 22:03:33 +0100

    replace package.json with procfile

--- cronyGlasses/settings.py
+++ cronyGlasses/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from django.core.management.utils import get_random_secret_key
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,7 +135,7 @@ MEDIA_ROOT='BASE_DIR/"static/images"'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
