9def1cfcf9659551d52c0ff38404f8f18609e4f8
Praise Freedom Dike <dikepraise119@gmail.com>
2023-04-03 12:58:46 +0100

    add changes in settings file

--- cronyGlasses/settings.py
+++ cronyGlasses/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'service.apps.ServiceConfig',
     'paypal.standard.ipn',
+    
 ]
 
 MIDDLEWARE = [
@@ -137,6 +138,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
