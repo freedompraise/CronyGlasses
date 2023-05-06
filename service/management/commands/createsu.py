449a138a30d2eb5fb800dbaa34cc8e88c56d9dc0
Praise Freedom Dike <dikepraise119@gmail.com>
2023-05-06 18:41:19 +0100

    change admin password

--- service/management/commands/createsu.py
+++ service/management/commands/createsu.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
+from decouple import config
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
-                username='admin',
-                password='complexpassword123'
+                username=config('username'),
+                password=config('password')
             )
         print('Superuser has been created.')
