617b98b0154f979aa8c8dfa90e22cabb144c352f
Praise Freedom Dike <dikepraise119@gmail.com>
2023-05-08 04:08:23 +0100

    update settings.py

--- cronyGlasses/settings.py
+++ cronyGlasses/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-MEDIA_ROOT='BASE_DIR/"static/images"'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'images')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
