eb285218baee774a7231fb2bacf70673549e8feb
Praise Freedom Dike <dikepraise119@gmail.com>
2023-09-05 10:12:01 +0100

    refactor payment_done views logic

--- service/views.py
+++ service/views.py
@@ -248,20 +248,12 @@ def paypal_checkout(request):
 
 
 def payment_done(request):
-    order = request.session.get('order')
-    if not order:
-        return redirect('home')
+    Order.objects.filter(user=request.user).delete()
+    cart = Cart.objects.get(user=request.user)
+    OrderItem.objects.filter(cart=cart).delete()
     
-    order_items = order.order_items.all()
-    total = order.get_total()
+    return redirect('home')
     
-    context = {
-        'order_items': order_items,
-        'total': total,
-    }
-    
-    del request.session['order']
-    return render(request, 'service/payment_done.html')
-
+   
 def payment_cancelled(request):
     return render(request, 'service/payment_cancelled.html')
