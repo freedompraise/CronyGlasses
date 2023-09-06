1c4bc5df3ee933033455af9db3c2225ea8ddc415
Praise Freedom Dike <dikepraise119@gmail.com>
2023-09-06 08:44:38 +0100

    define total as global variable

--- service/views.py
+++ service/views.py
@@ -19,22 +19,14 @@ from decimal import Decimal
 import random
 
 # Global Total variable handles a user havning no cart items yet
-total = 0
+def total(request):
+    return (sum(item.quantity for item in request.user.cart.order_items.all()) if request.user.is_authenticated else 0 )
 
 # View for the index page
 def index(request):
-    # Get the first 4 drinks in the database as popular products
     popular_products = Drink.objects.all()[:4]
-    # Get the next 4 drinks in the database as hot gifts
     hot_gifts = Drink.objects.all()[4:8]
-    total = 0
-    try:
-        # If the user is logged in, get the total number of items in their cart
-        total = sum(item.quantity for item in request.user.cart.order_items.all())
-    except:
-        pass
-    # Render the index.html template with the popular products, hot gifts, and cart total
-    return render(request,'service/index.html',{'total':total, 'popular':popular_products, 'hot':hot_gifts})
+    return render(request,'service/index.html',{'total':total(request), 'popular':popular_products, 'hot':hot_gifts})
 
 def related_products(product_id):
     drinks = Drink.objects.all()
@@ -46,66 +38,50 @@ def related_products(product_id):
 
 # View for the login page
 def login_view(request):
-    total = 0
     if request.method == 'POST':
-        # If the form was submitted, create an AuthenticationForm instance with the submitted data
         form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
-            # If the form is valid, get the user's email and password from the form's cleaned data
             email = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            # Authenticate the user with the provided email and password
             user = authenticate(request, email=email, password=password)
             if user is not None:
-                # If the user is authenticated, log them in and redirect them to the home page
                 login(request, user)
                 return redirect('home')
             else:
-                # If the user is not authenticated, show an error message
                 messages.error(request, 'Invalid email or password.')
         else:
-            # If the form is not valid, show an error message
             messages.error(request, 'Invalid email or password.')
     else:
-        # If the form was not submitted, create a new AuthenticationForm instance
         form = AuthenticationForm()
     # Render the login.html template with the form and cart total
-    return render(request, 'service/login.html', {'form': form, 'total': total})
+    return render(request, 'service/login.html', {'form': form, 'total': total(request)})
 
 
 # View for the registration page
 def register(request):
-    total = 0
     if request.method == 'POST':
-        # If the form was submitted, get the user's input data
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
         password = request.POST['password']    
         # Generate a random username for the user
         username = get_random_string(length=10)     
-        # Try to get a user with the provided email address
         user, created = User.objects.get_or_create(email=email, username=username, defaults={'first_name': first_name, 'last_name': last_name, 'password': password})
         if created:
-            # If the user was created, log them in and create a new cart for them
             login(request, user)
             Cart.objects.create(user=user)
             return redirect('home')
-        # If the user already exists, show an error message
         messages.error(request, 'User with email already exists')
         return redirect('register')
     # If the form was not submitted, render the register.html template with the cart total
-    return render(request, 'service/register.html',{'total': total})
+    return render(request, 'service/register.html',{'total': total(request) })
 
 
 # View for a drink's product page
 def product_page(request, pk):
-    if request.user.is_authenticated:
-        total = sum(item.quantity for item in request.user.cart.order_items.all())
-
     return render(request, 'service/product.html', {
         'drink': get_object_or_404(Drink, pk=pk),
-        'total': 0,
+        'total': total(request),
         'related_products':related_products(product_id=pk),
         'reviews': random.randint(1,500),
     })
@@ -130,14 +106,13 @@ def add_to_cart(request, drink_id):
 
 @login_required(login_url = 'login')
 def cart(request):
-    total = sum(item.quantity for item in request.user.cart.order_items.all())
     cart = get_object_or_404(Cart, user=request.user)
     cart_items = cart.order_items.all()
     cart.save()
     context = {
         'cart_items': cart_items,
         'cart': cart,
-        'total':total,
+        'total':total(request),
         'discount': round(Decimal('0.1') * cart.total),
     }
     return render(request, 'service/cart.html', context)
@@ -164,10 +139,6 @@ def order_item_update(request, order_item_id):
         if new_quantity.isdigit() and int(new_quantity) > 0:
             order_item.quantity = int(new_quantity)
             order_item.save()
-
-            cart.total = sum(item.total_price for item in cart.order_items.all())
-            cart.save()
-
             messages.success(request, f"{order_item.drink.name} quantity has been updated.")
         else:
             messages.warning(request, "Invalid quantity.")
@@ -189,7 +160,7 @@ def checkout(request):
         cart.save()
 
     context = {
-        'total': sum(item.quantity for item in request.user.cart.order_items.all()),
+        'total': total(request),
         'cart': cart,
         'checkout_total': cart.total + 10 # change name to total 
     }
@@ -224,14 +195,13 @@ def paypal_checkout(request):
 
     form = PayPalPaymentsForm(initial=paypal_dict)
         
-    return render(request, 'service/payment.html', { 'form':form, 'page':'done','paypal_total':paypal_total, 'total': total })
+    return render(request, 'service/payment.html', { 'form':form, 'page':'done','paypal_total':paypal_total, 'total': total(request) })
 
 
 def payment_done(request):
     Order.objects.filter(user=request.user).delete()
     cart = Cart.objects.get(user=request.user)
     OrderItem.objects.filter(cart=cart).delete()
-    
     return redirect('home')
     
    
