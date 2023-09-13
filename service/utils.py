9e4c970965c78cee99d4460da393bf1021782fed
Praise Freedom Dike <dikepraise119@gmail.com>
2023-09-13 11:44:06 +0100

    add util for creating several drinks

--- service/utils.py
+++ service/utils.py
@@ -24,7 +24,7 @@ def reviews():
 def create_test_user(username="testuser", password="testpassword"):
     return User.objects.create_user(username=username, password=password)
 
-def create_test_drink(name, price, description, image):
+def create_test_drink(name="Drink 1", price=None, description=None, image=None):
     return Drink.objects.create(name=name, price=price, description=description, image=image)
 
 def create_test_cart(user):
@@ -35,3 +35,9 @@ def create_test_order(user, total, status="draft"):
 
 def create_test_order_item(order, drink, quantity):
     return OrderItem.objects.create(order=order, drink=drink, quantity=quantity)
+
+def create_multiple_test_drinks(number_of_drinks):
+    drinks = []
+    for i in range(number_of_drinks):
+        drinks.append(create_test_drink(name=f"Drink {i}"))
+    return drinks
