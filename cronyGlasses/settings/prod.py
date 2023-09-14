51c957c80e8a6c720a23da5f3535fd3ea1ae1c99
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-09-14 13:16:05 -0700

    Configure workflow (#26)
    * add django settings module in env
    
    * specify the job to run for all prs targetted to master
    
    * export environmetn variable
    
    * fix typo on L45
    
    * fix typo in Environment Variables
    
    * delete job for settings environment variables
    
    * add secret key and db url to env
    
    * specify db using sqlite boleean
    
    * specify allowed_host in base_settings module
    
    * add necessary environment variables
    
    * fix typo in n'allowed_hosts'
    
    * compress jobs to one
    
    * move debug variable to base_settings
    
    * add dev settings to gitignore
    
    * change test settings to dev

--- cronyGlasses/settings/prod.py
+++ cronyGlasses/settings/prod.py
@@ -1,9 +1,6 @@
 from .base_settings import *
 import dj_database_url
 
-DEBUG = os.getenv("DEBUG", "0").lower() in ["true", "t", "1"]
-
-
 SECURE_HSTS_SECONDS = 315300
 SECURE_SSL_REDIRECT = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
