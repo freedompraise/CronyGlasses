9e95d03662c012944940e66c3b7fc80ffa6cdb05
Praise Freedom Dike <dikepraise119@gmail.com>
2023-09-14 21:19:27 +0100

    add dev.py to git

---
+++ cronyGlasses/settings/dev.py
@@ -0,0 +1,9 @@
+from .base_settings import *
+import dj_database_url
+
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
