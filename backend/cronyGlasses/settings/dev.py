d72cdde24e4a8d641231b78d7e39bc6eec05d2d5
Praise Freedom Dike <dikepraise119@gmail.com>
2023-09-26 05:28:20 +0100

    configure dev settings to use http

--- backend/cronyGlasses/settings/dev.py
+++ backend/cronyGlasses/settings/dev.py
@@ -2,3 +2,7 @@ from .base_settings import *
 import dj_database_url
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "http")
+APPEND_SLASH = True
+SECURE_SSL_REDIRECT = False
+USE_HTTPS = False
+SCHEME = "https://" if USE_HTTPS else "http://"
