733ab5fba58649a54c4172b40981b1d6159a740b
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-05 02:37:58 +0100

    uncomment purge option in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  purge: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Look for classes used in JS/JSX/TS/TSX files
-    "./public/index.html", // Look for classes used in the HTML file
-  ],
+  // purge: [
+  //   "./src/**/*.{js,jsx,ts,tsx}", // Look for classes used in JS/JSX/TS/TSX files
+  //   "./public/index.html", // Look for classes used in the HTML file
+  // ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
