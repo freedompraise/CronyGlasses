89dd3d93bb41ba93487afd81a238ab18d30d55e6
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-06 16:32:50 +0100

    rename app.js sto app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
-import Navbar from "./components/NavBar";
+import { NavBar, Footer } from "./components/INDEX";
 import "tailwindcss/tailwind.css";
 
 function App() {
   return (
     <div>
       <Router>
-        <Navbar />
+        <NavBar />
         <Switch>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/product/:id" exact element={Product} />
+          <Route path="/" exact component={Home} />
+          <Route path="/product/:id" exact component={Product} />
         </Switch>
       </Router>
     </div>
