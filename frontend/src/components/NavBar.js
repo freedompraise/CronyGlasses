f8f4182931e2b25c87f14020e09e8bd2b37a2cad
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-10-06 07:09:02 +0100

    Configure react (#43)
    * add necessary configurations for project
    
    * create home page
    
    * downgrade version of react-router-dom
    
    * uncomment purge option in tailwind config
    
    * use cronyglasses icon for favicon
    
    * install fontawesome brands
    
    * delete home folder package.json and package-lock.json files
    
    * update tailwind configuration
    
    * add free-brands-svg-icons to dependencies
    
    * move icon to assets

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  faXTwitter,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+import { faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="flex justify-between items-center text-white h-16"
+      style={{
+        backgroundColor: "rgb(63, 55, 55)",
+        height: "70px",
+        width: "100%",
+      }}
+    >
+      {/* Left section */}
+
+      <div className="flex items-center space-x-4 ml-8">
+        {/* Twitter Icon */}
+        <a href="https://twitter.com/freedom_praise">
+          <FontAwesomeIcon icon={faXTwitter} classname="fab fa-twitter" />
+        </a>
+        {/* GitHub Icon */}
+        <a href="https://github.com/freedompraise">
+          <FontAwesomeIcon icon={faGithub} classname="fab fa-github" />
+        </a>
+        {/* LinkedIn Icon */}
+        <a href="https://linkedin.com/in/praise-dike">
+          <FontAwesomeIcon icon={faLinkedin} classname="fab fa-linkedin" />
+        </a>
+      </div>
+
+      {/* Center section */}
+      <div className="flex items-center">
+        {/* Project Icon */}
+        <a href="/" className="flex items-center mr-6">
+          <img
+            src="assets/icon.png"
+            alt="Project Icon"
+            className="w-8 h-8 mr-2"
+          />
+          <span>CRONYGLASSES</span>
+        </a>
+      </div>
+
+      {/* Right section */}
+      <div className="flex items-center space-x-4 mr-8">
+        {/* User Icon */}
+        <i className="fas fa-user">
+          <FontAwesomeIcon icon={faUser} classname="fas fa-user" />
+        </i>
+        {/* Login */}
+        <span>Login</span>
+        {/* Cart Icon */}
+        <div className="flex items-center">
+          <i className="fas fa-shopping-cart">
+            <FontAwesomeIcon
+              icon={faShoppingCart}
+              classname="fas fa-shopping-cart"
+            />
+          </i>
+          <span className="ml-1">0</span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-blue-500">
+      <h1 className="text-white">Welcome to the Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
