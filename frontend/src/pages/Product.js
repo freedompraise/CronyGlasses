516aa6a70b472645c17219d2e6cd93f06985a136
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-06 07:14:48 +0100

    add code to product page

---
+++ frontend/src/pages/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Product() {
+  return (
+    <div className="bg-blue-500">
+      <h1 className="text-white">Welcome to the Product Page</h1>
+    </div>
+  );
+}
+
+export default Product;
