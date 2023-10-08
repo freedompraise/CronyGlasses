574d6d6dc93d9a65ac2bf7ee005eb3ca9804f664
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-09 00:46:31 +0100

    update drinks component name

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import banner from "../assets/banner.png";
-import Drink from "../components/Drink";
+import Drinks from "../components/Drinks";
 
 function Home() {
   return (
     <div className="">
       <img src={banner} alt="banner" className="w-full h-340" />
       <h3 className="text-black">This is Home Page</h3>
-      <Drink />
+      <Drinks />
     </div>
   );
 }
