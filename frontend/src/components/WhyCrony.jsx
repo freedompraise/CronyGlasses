90eabf9c7fd6378b545ad0359553e92f4d2934bd
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-10-10 11:10:52 +0100

    Complete homepage styling (#47)
    * divide drinks into popuar and tp producrts
    
    * create about page
    
    * render about page

---
+++ frontend/src/components/WhyCrony.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const WhyCrony = () => {
+  return (
+    <div className="max-w-6xl mx-auto my-8 p-4 text-center">
+      <h2 className="text-xl mb-4">WHY SHOP WITH CRONYGLASSES</h2>
+      <br />
+      <p className="text-base mb-4">
+        <strong>Quality Eyewear Selection:</strong> At CronyGlasses, we curate a
+        collection of eyewear that stands for quality, style, and durability.
+        Our frames are meticulously chosen to ensure they not only look great
+        but also provide utmost comfort and longevity.
+      </p>
+      <p className="text-base mb-4">
+        <strong>Affordable Pricing:</strong> We believe that everyone should
+        have access to high-quality eyewear without breaking the bank. Our
+        pricing is designed to be affordable, allowing customers to find the
+        perfect pair of glasses without compromising on their budget.
+      </p>
+      <p className="text-base mb-4">
+        <strong>Customer-Centric Approach:</strong> Putting our customers first
+        is our mantra. We listen to your needs, preferences, and feedback to
+        tailor our offerings and services, ensuring a delightful shopping
+        experience every time.
+      </p>
+      <p className="text-base mb-4">
+        <strong>Quick and Secure Delivery:</strong> We understand the excitement
+        of receiving a new pair of glasses. That's why we strive for swift and
+        secure deliveries, so you can start flaunting your new eyewear without
+        unnecessary delays.
+      </p>
+      <p className="text-base">
+        <strong>Personalized Style Consultations: </strong>
+        Unsure about the frame that suits you best? Our experts are here to
+        help. We offer personalized style consultations to guide you in choosing
+        eyewear that complements your features and lifestyle.
+      </p>
+    </div>
+  );
+};
+
+export default WhyCrony;
