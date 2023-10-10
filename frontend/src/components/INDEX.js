75a4e57e0301d61c1d62298367bdea815637187f
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-10 11:07:17 +0100

    create about page

--- frontend/src/components/INDEX.js
+++ frontend/src/components/INDEX.js
@@ -1,4 +1,5 @@
 export { default as Footer } from "../components/Footer";
 export { default as NavBar } from "../components/NavBar";
-
+export { default as WhyCrony } from "../components/WhyCrony";
 export { default as Loader } from "../components/Loader";
+export { default as Drinks } from "../components/Drinks";
