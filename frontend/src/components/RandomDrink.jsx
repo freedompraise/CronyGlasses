7505b154c483f3acc280e0d642840823a1c2f510
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-22 18:25:27 +0100

    add tailwind styling to drink component

--- frontend/src/components/RandomDrink.jsx
+++ frontend/src/components/RandomDrink.jsx
@@ -15,13 +15,39 @@ function RandomDrink() {
   }, []);
 
   return (
-    <div className="mx-0 h-full w-full">
-      <div className="flex mt-2 items-center justify-center h-full">
-        <img className="" src={randomDrink.image} alt={randomDrink.name} />
+    <div className="flex flex-row">
+      <div className="w-1/2">
+        <img src={randomDrink.image} alt={randomDrink.name} />
       </div>
-      <h2 className="mt-1 lg:text-xl text-black">{randomDrink.name}</h2>
-      <h3 className="text-lg font-bold mt-1">${randomDrink.price} </h3>
-      <p className="text-sm mt-1">{randomDrink.description}</p>
+      <div className="w-1/2 p-4">
+        <h2 className="text-3xl font-bold">{randomDrink.name}</h2>
+        <hr className="my-4" />
+        <h3 className="text-xl font-bold">${randomDrink.price}</h3>
+        <p className="text-lg my-4">{randomDrink.description}</p>
+        <ul className="list-disc list-inside">
+          <li>70cl bottle</li>
+          <li>All Natural Flavourings</li>
+          <li>ABV: {randomDrink.abv}% vol</li>
+        </ul>
+        <div className="flex flex-row my-4">
+          <label htmlFor="qty" className="mr-4">
+            Qty
+          </label>
+          <input
+            type="number"
+            id="qty"
+            name="qty"
+            placeholder="0"
+            className="border border-gray-400 rounded-md p-2 w-1/3"
+          />
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
+          Add to Cart
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Buy with Shop
+        </button>
+      </div>
     </div>
   );
 }
