4755c6671e35c52702494750a23ac0db1915d194
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-24 14:14:34 +0100

    create login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="bg-white p-4 md:p-8 mx-auto max-w-lg mt-8">
+      <h2 className="text-left text-lg font-bold uppercase mb-4">
+        CUSTOMER LOGIN
+      </h2>
+      <hr className="w-full mb-4" />
+      <form className="flex flex-col items-center">
+        <label htmlFor="email" className="text-left mb-2">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="border border-gray-400 rounded-md p-2 w-full mb-4"
+          required
+        />
+        <div className="flex flex-row ">
+          {" "}
+          <label htmlFor="password" className="text-left mb-2">
+            Password:
+          </label>
+          <a href="#" className="text-blue-500 hover:text-blue-700">
+            Forgot your password?
+          </a>{" "}
+        </div>
+
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className="border border-gray-400 rounded-md p-2 w-full mb-4"
+          required
+        />
+        <div className="flex justify-between w-full">
+          <button
+            type="submit"
+            className="bg-black hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+          >
+            Log In
+          </button>
+        </div>
+      </form>
+      <p className="mt-8 text-center">
+        New Customer?{" "}
+        <a href="#" className="text-blue-500 hover:text-blue-700">
+          Sign up
+        </a>
+      </p>
+    </div>
+  );
+};
+export default Login;
