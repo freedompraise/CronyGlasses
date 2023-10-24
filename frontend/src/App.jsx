4c0b9b85d1d7fa7751ec171b78796e42140af946
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2023-10-24 15:06:06 +0100

    update login style (#55)
    * make drinks appear in same row
    
    * specify max width for drinks container
    
    * convert whycrony heading to a question
    
    * combine popular and top drinks
    
    * comment tasks to do
    
    * create random-drink endpoint
    
    * create component for random drink
    
    * add tailwind styling to drink component
    
    * create subscribe component
    
    * update navbar bg to grey
    
    * complete homepage
    
    * upload image
    
    * import vdrink component
    
    * update drinks route
    
    * install tyoe-reacter-dom
    
    * create d.ts file
    
    * query drink from the drink page api
    
    * create faq component
    
    * create relateddrinks component
    
    * create login page
    
    * update login style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import Login from "./pages/Login";
 import { NavBar, Footer, Subscribe } from "./components/INDEX";
 import "tailwindcss/tailwind.css";
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/drinks/:id" exact component={Product} />
+          <Route path="/login" exact component={Login} />
         </Switch>
         <Subscribe />
         <Footer />
