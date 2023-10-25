5f7f55086db3a93aa1475778696d698a455ce913
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-25 05:29:42 +0100

    link navbar to cart

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -23,15 +23,24 @@ const NavBar = () => {
       <div className="flex items-center space-x-4 ml-8">
         {/* Twitter Icon */}
         <a href="https://twitter.com/freedom_praise">
-          <FontAwesomeIcon icon={faXTwitter} className="fab fa-twitter" />
+          <FontAwesomeIcon
+            icon={faXTwitter}
+            className="fab fa-twitter hover:text-black"
+          />
         </a>
         {/* GitHub Icon */}
         <a href="https://github.com/freedompraise">
-          <FontAwesomeIcon icon={faGithub} className="fab fa-github" />
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="fab fa-github hover:text-black"
+          />
         </a>
         {/* LinkedIn Icon */}
         <a href="https://linkedin.com/in/praise-dike">
-          <FontAwesomeIcon icon={faLinkedin} className="fab fa-linkedin" />
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className="fab fa-linkedin hover:text-black"
+          />
         </a>
       </div>
 
@@ -39,15 +48,15 @@ const NavBar = () => {
       <div className="flex flex-col items-center">
         {/* Project Icon */}
         <a href="/" className="flex items-center flex-col">
-          <img src={icon} alt="Project Icon" className="w-8 h-8 mr-2" />
+          <img src={icon} alt="Project Icon" className="w-8 h-8 mr-2 " />
           <div className="text-xl">CRONYGLASSES</div>
         </a>
       </div>
 
       {/* Right section */}
-      <div className="flex items-center space-x-2 mr-8">
+      <div className="flex items-center space-x-2 mr-8 ">
         {/* User Icon */}
-        <a href="/account">
+        <a href="/account" className="hover:text-black">
           <i className="fas fa-user">
             <FontAwesomeIcon icon={faUser} className="fas fa-user" />
           </i>
@@ -55,15 +64,17 @@ const NavBar = () => {
           <span>LOGIN</span>
         </a>
         {/* Cart Icon */}
-        <div className="flex items-center">
-          <i className="fas fa-shopping-cart">
-            <FontAwesomeIcon
-              icon={faShoppingCart}
-              className="fas fa-shopping-cart"
-            />
-          </i>
-          <span className="ml-1">0</span>
-        </div>
+        <a href="/cart" className="hover:text-black">
+          <div className="flex items-center">
+            <i className="fas fa-shopping-cart">
+              <FontAwesomeIcon
+                icon={faShoppingCart}
+                className="fas fa-shopping-cart"
+              />
+            </i>
+            <span className="ml-1">0</span>
+          </div>
+        </a>
       </div>
     </nav>
   );
