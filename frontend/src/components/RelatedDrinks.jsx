5a072aab648ff5940617547b7082cfdb08f909b1
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-31 10:39:01 +0100

    update p-tag styling for responsive design

--- frontend/src/components/RelatedDrinks.jsx
+++ frontend/src/components/RelatedDrinks.jsx
@@ -3,22 +3,31 @@ import React from "react";
 const RelatedDrinks = (props) => {
   return (
     <div className="bg-white p-4 md:p-8 mt-8">
-      <h2 className="text-center text-lg font-bold uppercase mb-4">
+      <h2 className="text-center text-lg font-mono uppercase mb-4">
         CHECK OUT OUR COLLECTIONS
       </h2>
       <hr className="w-full mb-4" />
-      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid sm:grid-cols-3 grid-cols-2 gap-4">
         {props.relatedDrinks &&
-          props.relatedDrinks
-            .slice(0, 3)
-            .map((drink, index) => (
+          props.relatedDrinks.slice(0, 3).map((drink, index) => (
+            <a
+              href={`/drinks/${drink.id}`}
+              key={index}
+              className="relative group"
+            >
               <img
-                key={index}
                 src={drink.image}
                 alt={`Drink ${index + 1}`}
-                className="mx-2"
+                className="mx-2 overflow-hidden rounded-lg shadow-lg"
               />
-            ))}
+              <div className="absolute inset-0 bg-black bg-opacity-0 transition-all duration-500 ease-in-out group-hover:bg-opacity-50 flex items-center justify-center">
+                <p className="text-3xl font-bold text-white font-sans text-center opacity-0 lg:block lg:group-hover:opacity-100">
+                  {drink.name}
+                </p>
+              </div>
+              <div className="text-center sm:hidden">{drink.name}</div>
+            </a>
+          ))}
       </div>
     </div>
   );
