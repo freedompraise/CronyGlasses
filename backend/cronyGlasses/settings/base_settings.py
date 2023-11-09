4f2a6f6a4fe9fb41603b52b848395910f9396cb0
Praise Freedom Dike <dikepraise119@gmail.com>
2023-11-09 09:13:05 +0100

    Fix cloudinary environment variables in base settings

--- backend/cronyGlasses/settings/base_settings.py
+++ backend/cronyGlasses/settings/base_settings.py
@@ -116,9 +116,9 @@ USE_I18N = True
 USE_TZ = True
 
 CLOUDINARY_STORAGE = {
-    "CLOUDINARY_CLOUD_NAME": os.getenv("CLOUD_NAME"),
-    "CLOUDINARY_API_KEY": os.getenv("API_KEY"),
-    "CLOUDINARY_API_SECRET": os.getenv("API_SECRET"),
+    "CLOUDINARY_CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "CLOUDINARY_API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "CLOUDINARY_API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
