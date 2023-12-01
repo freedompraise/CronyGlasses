36721dea62dca274db7f8682f8d8a371f8741f6e
Praise Freedom Dike <dikepraise119@gmail.com>
2023-12-01 11:42:56 +0100

    create cartcontext manger and apply wrap around app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,29 @@ import Signup from "./pages/Signup";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import { NavBar, Footer, Subscribe } from "./components/INDEX";
+import { CartContextProvider } from "./CartContext";
 import "tailwindcss/tailwind.css";
 
 function App() {
   return (
-    <div className="font-serif">
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/drinks/:id" exact component={Product} />
-          <Route path="/account/login" exact component={Login} />
-          <Route path="/account/signup" exact component={Signup} />
-          <Route path="/account" exact component={Account} />
-          <Route path="/cart" exact component={Cart} />
-          <Route path="/checkout" exact component={Checkout} />
-        </Switch>
-        <Subscribe />
-        <Footer />
-      </Router>
-    </div>
+    <CartContextProvider>
+      <div className="font-serif">
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/drinks/:id" exact component={Product} />
+            <Route path="/account/login" exact component={Login} />
+            <Route path="/account/signup" exact component={Signup} />
+            <Route path="/account" exact component={Account} />
+            <Route path="/cart" exact component={Cart} />
+            <Route path="/checkout" exact component={Checkout} />
+          </Switch>
+          <Subscribe />
+          <Footer />
+        </Router>
+      </div>
+    </CartContextProvider>
   );
 }
 
