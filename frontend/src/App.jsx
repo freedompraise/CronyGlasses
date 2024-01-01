93abe227347deb0049307a904c2dcef7535ed096
Praise Freedom Dike <dikepraise119@gmail.com>
2024-01-01 02:54:41 +0100

    Remove Signup page and related routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import Account from "./pages/Account";
-import Signup from "./pages/Signup";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import { NavBar, Footer, Subscribe } from "./components/INDEX";
@@ -21,7 +20,6 @@ function App() {
             <Route path="/" exact component={Home} />
             <Route path="/drinks/:id" exact component={Product} />
             <Route path="/account/login" exact component={Login} />
-            <Route path="/account/signup" exact component={Signup} />
             <Route path="/account" exact component={Account} />
             <Route path="/cart" exact component={Cart} />
             <Route path="/checkout" exact component={Checkout} />
