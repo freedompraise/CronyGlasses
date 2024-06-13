6d345d3ea9d1de94b81d14f7925326a435b5667c
Praise Freedom Dike <dikepraise119@gmail.com>
2024-06-13 10:53:23 +0100

    chore: Update ThankYou component to use new naming convention for OrderConfirmation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import Login from "./pages/Login";
-import Account from "./pages/Account";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import OrderConfirmation from "./pages/ThankYou";
+import {
+  Home,
+  Product,
+  Login,
+  Account,
+  Cart,
+  Checkout,
+  ThankYou as OrderConfirmation,
+} from "./pages";
 import { NavBar, Footer, Subscribe, PrivateRoute } from "./components";
 import { CartContextProvider } from "./CartContext";
 import "tailwindcss/tailwind.css";
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Account } from "./Account";
+export { default as Cart } from "./Cart";
+export { default as Checkout } from "./Checkout";
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as Product } from "./Product";
+export { default as ThankYou } from "./ThankYou";
