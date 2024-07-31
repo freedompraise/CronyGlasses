824f39a6070ee7c7317a6e56ab2cb770c18a57f4
Praise Freedom Dike <dikepraise119@gmail.com>
2024-07-31 19:32:17 +0100

    update login form layout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,60 +1,82 @@
-import React from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { toast } from "sonner";
 
-function Login () {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
     localStorage.setItem("userEmail", data.email);
-    console.log("Login successful!");
-    window.location.href = "/account";  
-  }
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-4 md:p-8 mx-auto max-w-lg my-12">
-      <h2 className="text-left text-2xl font-bold mb-4">
-        CUSTOMER LOGIN
-      </h2>
-      <hr className="w-full mb-4" />
-        <label htmlFor="email" className="mb-2">
-          Email:
-        </label>
-        <input
-        {...register("email", { required: true })}
-          type="email"
-          id="email"
-          name="email"
-          placeholder="johndoe@gmail.com"
-          className="border border-gray-400 rounded-md p-2 w-full mb-4"
-          required
-        />
-        <div className="flex justify-between w-full">
-          {" "}
-          <label htmlFor="password" className="text-left mb-2">
-            Password:
-          </label>
-          <p className="text-gray-800 font-mono ">
-            Generic Password is <strong className="hover:text-blue-700" >"password"</strong>
-          </p>{" "}
-        </div>
+    toast.success("Logged in successfully");
+    window.location.href = "/account";
+  };
 
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className="border border-gray-400 rounded-md p-2 w-full mb-4"
-          value="password"
-          required
-        />
-        <div className="flex justify-between w-full">
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="bg-white p-4 md:p-8 mx-auto max-w-lg my-12"
+    >
+      <h2 className="text-left text-2xl font-bold mb-4">CUSTOMER LOGIN</h2>
+      <hr className="w-full mb-4" />
+      <label htmlFor="email" className="mb-2">
+        Email:
+      </label>
+      <input
+        {...register("email", { required: true })}
+        type="email"
+        id="email"
+        name="email"
+        defaultValue={"johndoe@gmail.com"}
+        className="border border-gray-400 rounded-md p-2 w-full mb-4"
+        required
+      />
+      <div className="flex justify-between w-full">
+        <label htmlFor="password" className="text-left mb-2">
+          Password:
+        </label>
+        <div className="flex items-center">
           <button
-            type="submit"
-            className="bg-black hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+            type="button"
+            onClick={togglePasswordVisibility}
+            className="focus:outline-none"
           >
-            Log In
+            <FontAwesomeIcon
+              icon={showPassword ? faEyeSlash : faEye}
+              className="text-gray-600"
+            />
           </button>
-       
         </div>
-      </form>
+      </div>
+      <input
+        {...register("password", { required: true })}
+        type={showPassword ? "text" : "password"}
+        id="password"
+        name="password"
+        className="border border-gray-400 rounded-md p-2 w-full mb-4"
+        defaultValue={"password"}
+        required
+      />
+      <div className="flex justify-between w-full">
+        <button
+          type="submit"
+          className="bg-black hover:bg-gray-900 text-white font-bold py-2 px-4 rounded"
+        >
+          Log In
+        </button>
+      </div>
+    </form>
   );
-};
+}
+
 export default Login;
