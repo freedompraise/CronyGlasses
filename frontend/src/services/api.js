18828628b10fad1a32f3598525a22fd2bd72c548
Praise Freedom Dike <dikepraise119@gmail.com>
2024-07-31 01:32:28 +0100

    chore: Refactor API module to improve code organization and remove unused code

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,48 +1,38 @@
-import axios from "axios";
 import { supabase } from "./supabaseClient";
 
 export const getAllDrinks = async () => {
-  if (localStorage.getItem("drinks")) {
-    return JSON.parse(localStorage.getItem("drinks"));
-  } else {
-    try {
-      const response = await supabase.from("Drinks").select("*");
-      localStorage.setItem("drinks", JSON.stringify(response.data));
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const response = await supabase.from("Drinks").select("*");
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
 };
 
 export const getRelatedDrinks = async (currentDrinkId) => {
-  if (localStorage.getItem("drinks")) {
-    const drinks = JSON.parse(localStorage.getItem("drinks"));
-    const relatedDrinks = drinks.filter(
-      (drink) => drink.id !== currentDrinkId && drink.id >= 1 && drink.id <= 8
-    );
-    return relatedDrinks.slice(0, 3);
-  } else {
-    throw new Error("No drinks found in local storage");
+  try {
+    const response = await supabase
+      .from("Drinks")
+      .select("*")
+      .neq("id", currentDrinkId)
+      .limit(3);
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
 };
 
 export const getRandDrink = async () => {
   const randomDrinkId = Math.floor(Math.random() * 8) + 1;
-  if (localStorage.getItem("drinks")) {
-    const drinks = JSON.parse(localStorage.getItem("drinks"));
-    return drinks.filter((drink) => drink.id === randomDrinkId);
-  } else {
-    try {
-      const response = await supabase
-        .from("Drinks")
-        .select("*")
-        .eq("id", randomDrinkId);
-      localStorage.setItem("drinks", JSON.stringify(response.data));
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
+  try {
+    const response = await supabase
+      .from("Drinks")
+      .select("*")
+      .eq("id", randomDrinkId);
+    localStorage.setItem("drinks", JSON.stringify(response.data));
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
 };
 
